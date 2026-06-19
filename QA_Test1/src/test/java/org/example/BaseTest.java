2c603cb372bcb027f3dee111376896857fa5a22f
mariakirnitskaya <maria105@mail.ru>
2026-06-19 17:07:24 +0700

    some minor changes

--- QA_Test1/src/test/java/org/example/BaseTest.java
+++ QA_Test1/src/test/java/org/example/BaseTest.java
@@ -7,15 +7,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-
     WebDriver driver;
 
     @BeforeMethod
     public void open() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
-        options.addArguments("headless");
-        driver = new ChromeDriver();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
     }
 
